a34fe1c2a78288d85f2151275190d49c9f792e91
fastjrun <fastjrun@139.com>
2016-07-15 11:54:49 +0800

    update

--- Web/Main/src/main/java/com/fastjrun/demospring4/web/controller/UserController.java
+++ Web/Main/src/main/java/com/fastjrun/demospring4/web/controller/UserController.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.apache.ibatis.session.RowBounds;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.fastjrun.demospring4.bean.User;
@@ -21,21 +23,36 @@ public class UserController {
     BaseUserService baseUserService;
 
     @RequestMapping(value = "/index", method = RequestMethod.GET)
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("userName", "zhangsan");
         return "index";
     }
 
     @RequestMapping(value = "/list", method = RequestMethod.GET)
-    public String showUserList(@ModelAttribute("model") ModelMap model) {
-        RowBounds rowBounds = new RowBounds(0, 10);
+    public String showUserList(Model model, @RequestParam(required = false, defaultValue = "0") int offset,
+            @RequestParam(required = false, defaultValue = "30") int limit) {
+        RowBounds rowBounds = new RowBounds(offset, limit);
         List<User> list = baseUserService.queryForLimitList(rowBounds);
-        Integer totalCount = new Integer(baseUserService.totalCount());
+        int totalCount = baseUserService.totalCount();
         model.addAttribute("userList", list);
-        model.addAttribute("vpiym", totalCount);
+        model.addAttribute("totalCount", totalCount);
         return "list";
     }
 
-    @RequestMapping(value = "initadd", method = RequestMethod.GET)
+    @RequestMapping(value = "/mylist", method = RequestMethod.GET)
+    public ModelAndView home() {
+        RowBounds rowBounds = new RowBounds(0, 10);
+        List<User> list = baseUserService.queryForLimitList(rowBounds);
+        Integer totalCount = new Integer(baseUserService.totalCount());
+
+        ModelAndView mav = new ModelAndView("list");
+        mav.addObject("userList", list);
+        mav.addObject("vpiym", totalCount);
+
+        return mav;
+    }
+
+    @RequestMapping(value = "/initadd", method = RequestMethod.GET)
     public String init2add(@ModelAttribute("user") User user) {
 
         return "add";
