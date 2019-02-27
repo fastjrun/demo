4b0533fabbec4ba7d140c933df9560df9a1d712f
fastjrun <fastjrun@139.com>
2019-02-27 23:27:07 +0800

    修复测试数据，规范文件命名等

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ if [ "local_bundle_mock" = $1 ] ; then
     mvn clean package -pl demo-bundle-mock -am -Dbdmgc.skip=false
 elif [ "provider_mock" = $1 ] ; then
     mvn clean package -pl demo-provider-mock -am -Dbdmgc.skip=false
-    cp demo-provider-mock/target/demo-provider-mock.war /d/server/apache-tomcat-8.0.30/webapps/demo-provider-mock.war
+    cp demo-provider-mock/target/demo-provider-mock.war ~/server/tomcat8/webapps/demo-provider-mock.war
 elif [ "package_api_test" = $1 ] ; then
     mvn clean package -pl demo-api-test -am -Dclientgc.skip=false
 elif [ "unitTest" = $1 ] ; then
@@ -14,13 +14,13 @@ elif [ "mock_test" = $1 ] ; then
     mvn clean package -pl demo-api-test -Dclientgc.skip=false -Pmock
 elif [ "package_provider" = $1 ] ; then
     mvn clean package -pl demo-provider -am -Dbasegc.skip=false -Dbdgc.skip=false -P$2
-    cp demo-provider/target/demo-provider.war /d/server/apache-tomcat-8.0.30/webapps/demo-provider.war
+    cp demo-provider/target/demo-provider.war ~/server/tomcat8/webapps/demo-provider.war
 elif [ "api_test" = $1 ] ; then
     mvn clean package -pl demo-api-test -Dclientgc.skip=false -P$2
 elif [ "package_task" = $1 ] ; then
     mvn clean package -pl demo-task -am -Dbasegc.skip=false -Dbdgc.skip=false -P$2
-    rm -rf /d/app/demo/demo-task
-    cp -r demo-task/target/demo-task/demo-task /d/app/demo
+    rm -rf ~/app/demo/demo-task
+    cp -r demo-task/target/demo-task/demo-task ~/app/demo
 elif [ "package_ci" = $1 ] ; then
     mvn clean package -pl demo-base -am -Dbasegc.skip=false
     mvn clean package -pl demo-bundle -am -Dbdgc.skip=false
